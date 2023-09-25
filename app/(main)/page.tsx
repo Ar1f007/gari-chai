13b39d2d366308b1db0d47ddae225162ad881bde
Ariful Hoque Al Amin <muhammadarif9416@yahoo.com>
2023-09-25 15:47:24 +0600

    add: hero section image

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,3 +1,11 @@
+import Hero from "@/modules/home/hero";
+
 export default function Home() {
-  return null;
+  return (
+    <>
+      <section className="container mx-auto max-w-screen-2xl px-6">
+        <Hero />
+      </section>
+    </>
+  );
 }
---
+++ modules/home/hero/index.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+const Hero = () => {
+  return (
+    <div className="h-[70vh] w-full relative">
+      <Image
+        src="/hero.webp"
+        alt="Lady standing by a car"
+        priority
+        className="absolute inset-0 object-cover"
+        draggable={false}
+        fill
+        sizes="100vw"
+      />
+    </div>
+  );
+};
+
+export default Hero;
