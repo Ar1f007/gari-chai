759c68903e3465495c19aca9f7116cf77a0bec4f
Ariful Hoque Al Amin <muhammadarif9416@yahoo.com>
2023-09-25 18:36:13 +0600

    update: convert tailwind config to ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import { nextui } from "@nextui-org/theme";
+import { Config } from "tailwindcss";
 
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -19,3 +19,5 @@ module.exports = {
   darkMode: "class",
   plugins: [nextui()],
 };
+
+export default config;
