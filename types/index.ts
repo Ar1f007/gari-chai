1a53bf905776f28e23d59974e4d4805d2c160ee9
Ariful Hoque Al Amin <muhammadarif9416@yahoo.com>
2023-10-13 01:46:49 +0600

    Initial commit from Create Next App

--- types/index.ts
+++ types/index.ts
@@ -1,21 +1,5 @@
-import { SVGProps } from "react";
+import {SVGProps} from "react";
 
 export type IconSvgProps = SVGProps<SVGSVGElement> & {
   size?: number;
 };
-
-export type TabItem = {
-  id: string;
-  src: string;
-  name: string;
-  priceMin: string;
-  priceMax: string;
-};
-
-export type Tab = {
-  id: string;
-  brandName: string;
-  content: Array<TabItem>;
-};
-
-export type Tabs = Array<Tab>;
