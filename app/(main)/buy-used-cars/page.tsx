24dcac0f58b47a983a7b8d0e4564ba3ff4b055c9
Ariful Hoque Al Amin <muhammadarif9416@yahoo.com>
2023-10-27 09:54:48 +0600

    add: pages

---
+++ app/(main)/buy-used-cars/page.tsx
@@ -0,0 +1,10 @@
+import { Section } from '@/components/layout/section';
+
+const BuyUsedCarsPage = () => {
+  return (
+    <Section classNames='py-10'>
+      <p className='text-center'>Coming Soon</p>
+    </Section>
+  );
+};
+export default BuyUsedCarsPage;
---
+++ app/(main)/sell-cars/page.tsx
@@ -0,0 +1,10 @@
+import { Section } from '@/components/layout/section';
+
+const SellCarsPage = () => {
+  return (
+    <Section classNames='py-10'>
+      <p className='text-center'>Coming Soon</p>
+    </Section>
+  );
+};
+export default SellCarsPage;
