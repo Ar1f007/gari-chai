32c61f3f1418eb3f0abffd6d747a665b6d568d57
Ariful Hoque Al Amin <muhammadarif9416@yahoo.com>
2023-10-31 23:59:03 +0600

    update: constant val

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,7 +2,10 @@ import { z } from 'zod';
 
 export const MAX_FILE_SIZE_LIMIT = 300000; // 300 kb
 
-export const API_BASE_URL = 'http://localhost:8000';
+export const API_BASE_URL =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:8000'
+    : 'https://gari-chai.onrender.com';
 
 export const API_V1_URL = API_BASE_URL + '/api/v1';
 
