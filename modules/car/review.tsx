d3dc9d44d4f16a0df3f6dafc178ce9922c856d99
Ariful Hoque Al Amin <muhammadarif9416@yahoo.com>
2023-11-17 05:41:58 +0600

    add: date on review

--- modules/car/review.tsx
+++ modules/car/review.tsx
@@ -1,9 +1,11 @@
+import dayjs from 'dayjs';
+
 import { Ratings } from '@/components/ratings';
 import { ReviewBody } from '@/schema/review';
 
 const Review = ({ review }: { review: ReviewBody }) => {
   return (
-    <blockquote className='flex h-full flex-col justify-between rounded bg-default-100 p-6'>
+    <blockquote className='flex h-full flex-col justify-between overflow-hidden rounded-2xl border-1 border-solid border-gray-300 bg-default-100 p-6'>
       <h4
         className='mb-2 truncate text-xl font-medium'
         title='Excellent car'
@@ -22,7 +24,12 @@ const Review = ({ review }: { review: ReviewBody }) => {
         </p>
       </div>
 
-      <footer className='mt-8 text-sm text-gray-500'>&mdash; {review.userInfo.name}</footer>
+      <footer className='mt-8 flex flex-col gap-1'>
+        <span className='text-base font-medium text-gray-700'>By {review.userInfo.name}</span>
+        <span className='text-sm text-gray-500'>
+          on {dayjs(review.updatedAt).format('MMM MM, YYYY')}
+        </span>
+      </footer>
     </blockquote>
   );
 };
