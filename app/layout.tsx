dd11bf8256569d71be3f637f27ad438f98152b12
Ariful Hoque Al Amin <muhammadarif9416@yahoo.com>
2023-11-20 22:41:39 +0600

    html class

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,11 +32,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html
       lang='en'
       suppressHydrationWarning
+      className='light'
     >
       <head />
       <body className={clsx('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>
         <Providers themeProps={{ attribute: 'class', defaultTheme: 'light' }}>
-          <main className='bg-background text-foreground light'>{children}</main>
+          <main>{children}</main>
         </Providers>
       </body>
     </html>
