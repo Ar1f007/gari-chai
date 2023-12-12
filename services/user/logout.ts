0433fee717598121c7470293fd37636cfe83e79e
Ariful Hoque Al Amin <muhammadarif9416@yahoo.com>
2023-12-12 11:38:09 +0600

    update: logout using remove cookie route handler

--- services/user/logout.ts
+++ services/user/logout.ts
@@ -1,8 +1,12 @@
+import { AUTH_TOKEN_NAME } from '@/lib/constants';
 import { userActions } from '@/store';
 
 export async function logout() {
   userActions.setUser(null);
 
-  const res = await fetch('/api/logout');
+  const res = await fetch('/api/remove-cookie', {
+    method: 'POST',
+    body: JSON.stringify({ tokenName: AUTH_TOKEN_NAME }),
+  });
   console.log('s', await res.json());
 }
