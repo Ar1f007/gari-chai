d85b0271b790ad26f2e8bdf376411de0652dc135
Ariful Hoque Al Amin <muhammadarif9416@yahoo.com>
2024-01-22 00:33:04 +0600

    fix: on logout dropdown wheel remain

--- app/set-store-value.tsx
+++ app/set-store-value.tsx
@@ -1,23 +1,18 @@
 'use client';
 
 import { auth } from '@/services/user';
-import { userActions, userStore } from '@/store';
+import { userActions } from '@/store';
 import { useEffect } from 'react';
-import { useSnapshot } from 'valtio';
 
 const SetStoreValue = () => {
-  const userSnap = useSnapshot(userStore);
-
   async function getUser() {
     const res = await auth.me();
     userActions.setUser(res);
   }
 
   useEffect(() => {
-    if (!userSnap.user) {
-      getUser();
-    }
-  }, [userStore.user]);
+    getUser();
+  }, []);
 
   return null;
 };
