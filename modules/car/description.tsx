8780a68d0f97e771922dcfa18c75259deb510211
Ariful Hoque Al Amin <muhammadarif9416@yahoo.com>
2024-01-28 23:22:46 +0600

    add: toggle showmore showless button for description

--- modules/car/description.tsx
+++ modules/car/description.tsx
@@ -1,7 +1,32 @@
-const CarDescription = ({ text }: { text: string }) => {
+'use client';
+
+import { Button } from '@nextui-org/button';
+import { useState } from 'react';
+
+const CarDescription = ({ text }: { text: string }): JSX.Element => {
+  const [showSlicedVersion, setShowSlicedVersion] = useState<boolean>(text.length > 500);
+  const [buttonLabel, setButtonLabel] = useState<string>(showSlicedVersion ? 'more' : 'less');
+  const isTextLong = text.length > 500;
+
+  const toggleShowSlicedVersion = (): void => {
+    setShowSlicedVersion((prev) => !prev);
+    setButtonLabel(showSlicedVersion ? 'less' : 'more');
+  };
+
+  const slicedText = showSlicedVersion ? text.substring(0, 500) : text;
+
   return (
     <div className='mt-8 flex flex-col space-y-4 rounded-xl bg-foreground-50 p-6'>
-      <p>{text}</p>
+      <p>{slicedText}</p>
+
+      {isTextLong && (
+        <Button
+          onClick={toggleShowSlicedVersion}
+          className='w-fit'
+        >
+          Show {buttonLabel}
+        </Button>
+      )}
     </div>
   );
 };
