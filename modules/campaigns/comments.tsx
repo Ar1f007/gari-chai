5aec6ecb78d5bf41a66a5827df0131a14694bfb1
Ariful Hoque Al Amin <muhammadarif9416@yahoo.com>
2024-02-22 02:57:46 +0600

    add: bird svg

--- modules/campaigns/comments.tsx
+++ modules/campaigns/comments.tsx
@@ -5,7 +5,7 @@ import { cn } from '@/lib/utils';
 import { campaignStore, settingsStore } from '@/store';
 import { formatAsBangladeshiCurrency } from '@/util/covert-currency';
 import { Avatar } from '@nextui-org/avatar';
-import { PinIcon } from 'lucide-react';
+import { BirdIcon, PinIcon } from 'lucide-react';
 import { usePathname } from 'next/navigation';
 import { useSnapshot } from 'valtio';
 
@@ -30,33 +30,60 @@ const CampaignComments = () => {
         </h2>
 
         <ul className='mt-8 flex flex-col gap-5 xl:px-8'>
-          {campaignSnap.comments.slice(0, 3).map((item, idx) => (
-            <li
-              className='flex flex-wrap items-center justify-between gap-5'
-              key={item._id}
-            >
-              <div className='flex items-center gap-4'>
-                <Avatar
-                  src={item.user.profilePicture}
-                  isBordered
-                  color={idx == 0 ? 'primary' : idx == 1 ? 'secondary' : 'danger'}
-                  name={item.user.firstName + item.user.lastName}
-                  showFallback
-                />
-
-                <h3
-                  className={cn(
-                    'font-semibold',
-                    idx == 0 ? 'text-primary' : idx == 1 ? 'text-secondary' : 'text-danger',
-                  )}
-                >
-                  {item.user.firstName} {item.user.lastName}
-                </h3>
-              </div>
-
-              <p className='font-bold text-primary'>{formatAsBangladeshiCurrency(item.content)}</p>
+          {campaignSnap.comments.length === 0 ? (
+            <li>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                width='100'
+                height='100'
+                viewBox='0 0 24 24'
+                fill='none'
+                stroke='currentColor'
+                stroke-width='1'
+                stroke-linecap='round'
+                stroke-linejoin='round'
+                className='lucide lucide-bird mx-auto'
+                style={{ transform: 'rotateY(180deg)' }}
+              >
+                <path d='M16 7h.01' />
+                <path d='M3.4 18H12a8 8 0 0 0 8-8V7a4 4 0 0 0-7.28-2.3L2 20' />
+                <path d='m20 7 2 .5-2 .5' />
+                <path d='M10 18v3' />
+                <path d='M14 17.75V21' />
+                <path d='M7 18a6 6 0 0 0 3.84-10.61' />
+              </svg>
             </li>
-          ))}
+          ) : (
+            campaignSnap.comments.slice(0, 3).map((item, idx) => (
+              <li
+                className='flex flex-wrap items-center justify-between gap-5'
+                key={item._id}
+              >
+                <div className='flex items-center gap-4'>
+                  <Avatar
+                    src={item.user.profilePicture}
+                    isBordered
+                    color={idx == 0 ? 'primary' : idx == 1 ? 'secondary' : 'danger'}
+                    name={item.user.firstName + item.user.lastName}
+                    showFallback
+                  />
+
+                  <h3
+                    className={cn(
+                      'font-semibold',
+                      idx == 0 ? 'text-primary' : idx == 1 ? 'text-secondary' : 'text-danger',
+                    )}
+                  >
+                    {item.user.firstName} {item.user.lastName}
+                  </h3>
+                </div>
+
+                <p className='font-bold text-primary'>
+                  {formatAsBangladeshiCurrency(item.content)}
+                </p>
+              </li>
+            ))
+          )}
         </ul>
       </div>
       <div className='h-fit rounded-lg border px-2 py-5 shadow-sm md:p-5'>
@@ -65,31 +92,55 @@ const CampaignComments = () => {
         </div>
         <ul
           className={cn(
-            'mt-3 flex max-h-[350px] flex-col gap-2 overflow-y-auto',
+            'mt-3 flex max-h-[350px] min-h-[285px] flex-col gap-2 overflow-y-auto',
             campaignSnap.comments.length > 4 && 'pr-3',
           )}
         >
-          {campaignSnap.comments.map((item) => (
-            <li
-              className='flex flex-wrap items-center justify-between gap-5 rounded-md border bg-default-50 px-2 py-3 xl:px-8'
-              key={item._id}
-            >
-              <div className='flex items-center gap-4'>
-                <Avatar
-                  src={item.user.profilePicture}
-                  name={item.user.firstName}
-                  showFallback
-                  isBordered
-                />
-
-                <h3 className='font-semibold'>
-                  {item.user.firstName} {item.user.lastName}
-                </h3>
-              </div>
-
-              <p className='font-bold'>{formatAsBangladeshiCurrency(item.content)}</p>
+          {campaignSnap.comments.length === 0 ? (
+            <li className='py-10'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                width='100'
+                height='100'
+                viewBox='0 0 24 24'
+                fill='none'
+                stroke='currentColor'
+                stroke-width='1'
+                stroke-linecap='round'
+                stroke-linejoin='round'
+                className='lucide lucide-bird mx-auto'
+              >
+                <path d='M16 7h.01' />
+                <path d='M3.4 18H12a8 8 0 0 0 8-8V7a4 4 0 0 0-7.28-2.3L2 20' />
+                <path d='m20 7 2 .5-2 .5' />
+                <path d='M10 18v3' />
+                <path d='M14 17.75V21' />
+                <path d='M7 18a6 6 0 0 0 3.84-10.61' />
+              </svg>
             </li>
-          ))}
+          ) : (
+            campaignSnap.comments.map((item) => (
+              <li
+                className='flex flex-wrap items-center justify-between gap-5 rounded-md border bg-default-50 px-2 py-3 xl:px-8'
+                key={item._id}
+              >
+                <div className='flex items-center gap-4'>
+                  <Avatar
+                    src={item.user.profilePicture}
+                    name={item.user.firstName}
+                    showFallback
+                    isBordered
+                  />
+
+                  <h3 className='font-semibold'>
+                    {item.user.firstName} {item.user.lastName}
+                  </h3>
+                </div>
+
+                <p className='font-bold'>{formatAsBangladeshiCurrency(item.content)}</p>
+              </li>
+            ))
+          )}
         </ul>
       </div>
     </div>
