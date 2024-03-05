5cd43bce5e11725cd0b8ac704cdb7bb091dbca1f
Ariful Hoque Al Amin <muhammadarif9416@yahoo.com>
2024-03-05 12:14:17 +0600

    add: password reset

--- services/user/password-reset.ts
+++ services/user/password-reset.ts
@@ -1,26 +1,25 @@
-import {apiFetch, endpoints, ReqMethod} from "@/services";
-import {ResetPasswordPayload} from "@/schema/user";
+import { apiFetch, endpoints, ReqMethod } from '@/services';
+import { ResetPasswordPayload } from '@/schema/user';
 
 type ResetPasswordCodeParams = {
-    type: string;
-    sendCodeTo: string;
-    requestedFrom: string;
+  type: string;
+  sendCodeTo: string;
+  requestedFrom: string;
 };
 export async function resetPasswordCode(params: ResetPasswordCodeParams) {
-    const url = endpoints.api.users.resetPasswordCode;
+  const url = endpoints.api.users.resetPasswordCode;
 
-    return apiFetch(url, {
-        method: ReqMethod.POST,
-        body: params,
-    });
+  return apiFetch(url, {
+    method: ReqMethod.POST,
+    body: params,
+  });
 }
 
-
 export async function resetPassword(params: ResetPasswordPayload) {
-    const url = endpoints.api.users.resetPassword;
+  const url = endpoints.api.users.resetPassword;
 
-    return apiFetch(url, {
-        method: ReqMethod.POST,
-        body: params,
-    });
+  return apiFetch(url, {
+    method: ReqMethod.POST,
+    body: params,
+  });
 }
