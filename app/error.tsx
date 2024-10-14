fd86db47a85eaed9d5bbed86daf9eac1475ec10d
ar1f007 <muhammadarif9416@yahoo.com>
2024-10-15 03:55:19 +0600

    update: error message

--- app/error.tsx
+++ app/error.tsx
@@ -21,11 +21,19 @@ export default function Error({
 
   return (
     <div className='container flex h-screen flex-col items-center justify-center gap-8 p-5 text-center'>
-      <h1 className={title({ size: 'md' })}>{message}</h1>
 
-      <p className='font-medium md:text-xl lg:text-2xl'>
-        Don&apos;t worry, our team is already on it and working to fix the issue.
+      <h1 className={title({ size: 'sm', })}>
+        {process.env.NODE_ENV === 'production'
+          ? "Our API is currently waking up after a period of inactivity."
+          : message}
+      </h1>
+
+      <p className='font-medium md:text-xl max-w-4xl text-gray-700'>
+        {process.env.NODE_ENV === 'production'
+          ? "This temporary downtime is due to using a free-tier API during development. It should be back online shortly (within 15-25sec). Please try again in a moment!"
+          : "Don't worry, our team is already on it and working to fix the issue."}
       </p>
+
       <Button
         onClick={reset}
         variant='flat'
